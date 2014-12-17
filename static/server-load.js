da709a1ddce909beec02f95301ec6435057db8f8
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-12-17 17:09:16 +0100

    Added memory and swap timeline

--- static/server-load.js
+++ static/server-load.js
@@ -3,25 +3,32 @@ var millisPerLine = 10*60*1000
 var millisPerPoint = 500
 var millisPerPixel = 20
 
+var redSeries = { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.5)', lineWidth: 3 };
+var greenSeries = { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.5)', lineWidth: 3 };
+var blueSeries = { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.5)', lineWidth: 3 };
+
 var seriesOptions = [
-  { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(255, 255, 0, 1)', fillStyle: 'rgba(255, 255, 0, 0.1)', lineWidth: 3 }
+  { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.4)', lineWidth: 3 },
+  { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 },
+  { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.4)', lineWidth: 3 },
+  { strokeStyle: 'rgba(255, 255, 0, 1)', fillStyle: 'rgba(255, 255, 0, 0.4)', lineWidth: 3 }
 ];
 
 
 var dataSets = initDataSets();
 var state = {timestamp: null, cont:0, wait:null, stolen:null, sys:null, user:null}
+var memState = {timestamp: null, cont:0, wait:null, stolen:null, sys:null, user:null}
 
 function initDataSets(){
 
     var ds = {}
 
     ds.cpuData = []
+    ds.swapUsedDataSets = new TimeSeries()
+    ds.memUsedDataSets = new TimeSeries()
 
     ds.update = function(obj){
- 
+        console.log(obj)
         if (state.timestamp != obj.timestamp) {
             state.timestamp = obj.timestamp;
             state.cont = 0;
@@ -39,16 +46,17 @@ function initDataSets(){
         }else if (obj.name == nameDesc + ".cpu.user") {
             state.cont = state.cont + 1;
             state.user = parseFloat(obj.value, 10);
+        }else if (obj.name == nameDesc + ".mem.actualusedpercent"){
+            this.memUsedDataSets.append(obj.timestamp*1000, parseFloat(obj.value, 10));
+        }else if (obj.name == nameDesc + ".swap.usedpercent"){
+            this.swapUsedDataSets.append(obj.timestamp*1000, parseFloat(obj.value, 10));
         }
 
        if (state.cont == 4){
-           console.log(" values:",state);
-
            this.cpuData[3].append(state.timestamp*1000, state.wait);
            this.cpuData[2].append(state.timestamp*1000, state.wait + state.stolen);
            this.cpuData[1].append(state.timestamp*1000, state.wait + state.stolen + state.sys);
            this.cpuData[0].append(state.timestamp*1000, state.wait + state.stolen + state.sys + state.user);
-
        }
     }
     return ds;
@@ -62,10 +70,20 @@ function tick(value) {
 
 function init() {
   dataSets.cpuData = initHost('host1');
-  // initHost('host3');
-  // initHost('host4');
+  initMemData('host1')
 }
    
+function initMemData(hostId){
+  // Build the timeline
+  var timeline = new SmoothieChart(
+    { maxValue:100.00, minValue: 0.00,labels:{fillStyle:'#fff',fontSize:14},
+        millisPerPixel: millisPerPixel,timestampFormatter:SmoothieChart.timeFormatter,
+      grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: millisPerLine, verticalSections: 4 }});
+  
+  timeline.addTimeSeries(dataSets.memUsedDataSets, blueSeries);
+  timeline.addTimeSeries(dataSets.swapUsedDataSets, redSeries);
+  timeline.streamTo(document.getElementById(hostId + 'Mem'), millisPerPoint);
+}
 
 
 function initHost(hostId) {
