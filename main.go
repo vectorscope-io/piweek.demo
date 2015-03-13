0b52554a22db0e13511070f4b5bd9c95fde63073
regiluze <regiluze@hotmail.com>
2015-03-14 00:03:17 +0100

    unused code removed

--- main.go
+++ main.go
@@ -5,24 +5,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-
-	"github.com/aleasoluciones/serverstats"
 )
 
 func main() {
 	flag.Parse()
-
-	hub := NewWSHub()
-	serverStats := serverstats.NewServerStats(serverstats.DefaultPeriodes)
-	go func() {
-		for metric := range serverStats.Metrics {
-			b, _ := json.Marshal(metric)
-			hub.Broadcast(b)
-		}
-	}()
-
 	server := NewServer()
 	server.start()
 }
